8642cd103485eac3601ef778a1aa4b8f9e93aeda
Taha <taha.elmagbri2014@gmail.com>
2022-03-25 23:36:16 -0400

    commit tree 4e2d2f491a080ad855e0284d54dbcd010bb8d424

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats, name];
+    return copyOfCats;
+}
+
+function prependCat(name) {
+    const copyOfCats = [name, ...cats];
+    return copyOfCats;
+}
+
+function removeLastCat(name) {
+    const copyOfCats = cats.slice(0, -1);
+    return copyOfCats;
+}
+
+function removeFirstCat(name) {
+    const copyOfCats = cats.slice(1);
+    return copyOfCats;
+}
